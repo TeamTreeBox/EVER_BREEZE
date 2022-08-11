40798a2df225c27ca8fe6230f3ee3921a39579be
Kang JiYoon <rjal231@naver.com>
2022-08-11 19:07:40 +0900

    Book Image 추가

--- EverBreeze/Assets/1. JY/02.Scripts/BookVR.cs
+++ EverBreeze/Assets/1. JY/02.Scripts/BookVR.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class BookVR : MonoBehaviour
 {
+    //Book UI
     public GameObject book;
+    //Book이 나타나는 위치
     public GameObject anchor;
+    //UI 감지
     bool UIActive;
 
     void Start()
@@ -18,14 +21,19 @@ public class BookVR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //X버튼을 누를 시
         if (OVRInput.GetDown(OVRInput.Button.Three))
         {
+            //UI가 false면 true로 바꿔준다.
             UIActive = !UIActive;
+            //UI True
             book.SetActive(UIActive);
 
         }
+        //Book이 나타나는 Position
         if (UIActive)
         {
+            //Book Anchor의 위치 = Left Hand의 위치 
             book.transform.position = anchor.transform.position;
             book.transform.eulerAngles = new Vector3(anchor.transform.eulerAngles.x, anchor.transform.eulerAngles.y, 0);
         }
