97e46a0900caa5dfb3adc5c45ac14281d1dab422
HyunMin Lee <hmlee4135@naver.com>
2022-08-12 17:02:53 +0900

    Ray Grab
    1. 레이캐스트로 그랩 성공

--- EverBreeze/Assets/2. HM/2. Scripts/HM_RayGrab.cs
+++ EverBreeze/Assets/2. HM/2. Scripts/HM_RayGrab.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class HM_RayGrab : MonoBehaviour
 {
+   RaycastHit hit;
+    int layerMask;
+
+    public GameObject grapPos;
+    public GameObject grapable;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        layerMask = 1 << 6;        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            if(Physics.Raycast(transform.position,transform.forward,out hit, 300f, layerMask))
+            {
+                print("hit!");
+                Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
+
+                grapable = hit.transform.gameObject;
+
+                grapable.transform.SetParent(grapPos.transform);
+
+                grapable.transform.localPosition = Vector3.zero;
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.forward * 300f, Color.red);
+            }
+        }
     }
 }
