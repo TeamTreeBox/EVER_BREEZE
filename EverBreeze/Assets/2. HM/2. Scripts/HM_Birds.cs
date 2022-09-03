e84f992fa65faab3db937fab55a9aecbb92a51e5
HyunMin Lee <hmlee4135@naver.com>
2022-09-04 02:05:31 +0900

    0904 작업중

--- EverBreeze/Assets/2. HM/2. Scripts/HM_Birds.cs
+++ EverBreeze/Assets/2. HM/2. Scripts/HM_Birds.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class HM_Birds : MonoBehaviour
 {
     public GameObject Player;
-
+    public GameObject[] birdsMove;
     Animator birds_anim;
+    NavMeshAgent birds_Ai;
 
-    float distance;
+    public float distance;
+    public float aiDis;
 
+    public float XRange;
+    public float YRange;
+    public float ZRange;
+
+    public int a;
+    bool isPlayerCom = false;
+    bool isWalking = true;
+
+    Coroutine co_idle;
+    Coroutine co_Move;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
         birds_anim = GetComponent<Animator>();
+        birds_Ai = GetComponent<NavMeshAgent>();
+        XRange = Random.Range(-100, 100);
+        ZRange = Random.Range(-100, 100);
+        YRange = Random.Range(30, 60);
+
+        a = Random.Range(0, birdsMove.Length);
+
+      
+
+        co_idle = StartCoroutine(IsIdle());
 
     }
 
@@ -21,10 +46,82 @@ public class HM_Birds : MonoBehaviour
     void Update()
     {
         distance = Vector3.Distance(this.transform.position, Player.transform.position);
+        aiDis = Vector3.Distance(this.transform.position, birdsMove[a].transform.position);
 
-        if(distance < 10f)
+        if(distance < 10)
         {
-            birds_anim.SetBool("IsPlayerCome", true);
+            isPlayerCom = true;
+        }
+
+        if (isPlayerCom == true)
+        {
+            if(isWalking == true)
+            {
+            StopCoroutine(co_idle);
+            }
+            else
+            {
+            StopCoroutine(co_Move);
+            }
+
+            birds_Ai.enabled = false;
+            isPlayerCom = true;
+            IsPlayerCome();
         }
     }
+
+    void IsPlayerCome()
+    {
+        this.transform.LookAt(new Vector3(XRange, 10, ZRange));
+
+       
+        birds_anim.SetBool("IsWalk", false);
+        birds_anim.SetBool("IsPlayerCome", true);
+
+       // this.transform.Translate(new Vector3(XRange, YRange, ZRange) * speed * Time.deltaTime);
+
+       this.transform.position = Vector3.Lerp(this.transform.position,new Vector3(XRange, YRange, ZRange), speed * Time.deltaTime);
+
+
+        Destroy(this.gameObject, 10f);
+    }
+
+
+    IEnumerator IsIdle()
+    {
+        if(isPlayerCom == false)
+        {
+            isWalking = true;
+            yield return new WaitForSeconds(1f);
+
+            birds_anim.SetBool("IsPlayerCome", false);
+            birds_anim.SetBool("IsWalk", true);
+
+            birds_Ai.isStopped = false;
+
+            birds_Ai.SetDestination(birdsMove[a].transform.position);
+
+            if(aiDis < 1f)
+            {
+                isWalking = false;
+                co_Move = StartCoroutine(IsNextMove());
+            }
+            else
+            {
+                co_idle = StartCoroutine(IsIdle());
+            }
+
+        }
+
+    }
+
+    IEnumerator IsNextMove()
+    {
+        birds_Ai.isStopped = true;
+        birds_anim.SetBool("IsWalk", false);
+        yield return new WaitForSeconds(1f);
+        a = Random.Range(0, birdsMove.Length);
+        StartCoroutine(IsIdle());
+    }
+
 }
