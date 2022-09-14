a96dc4d048c7e0d43f9f92cbecad7bf6076e9cd9
HyunMin Lee <hmlee4135@naver.com>
2022-09-14 15:08:44 +0900

    플레이어 화면 페이드인 아웃 적용

---
+++ EverBreeze/Assets/2. HM/2. Scripts/HM_PlayerCameraFadeout.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HM_PlayerCameraFadeout : MonoBehaviour
+{
+
+    public GameObject Quad;
+
+    MeshRenderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = Quad.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartFadeIn()
+    {
+        StartCoroutine(GetFadeIn());
+    }
+
+    IEnumerator GetFadeOut()
+    {
+        Color C = render.material.color;
+
+        for(float i = 1; i >= 0; i -= 0.1f)
+        {
+            C.a = i;
+            render.material.color = C;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        C.a = 0;
+        render.material.color = C;
+    }
+
+    IEnumerator GetFadeIn()
+    {
+        Color C = render.material.color;
+
+        for (float i = 0; i <= 1; i += 0.1f)
+        {
+            C.a = i;
+            render.material.color = C;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        C.a = 1;
+        render.material.color = C;
+
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(GetFadeOut());
+    }
+}
